ea73f65cef6de3a49feffc29328b721807a0eb6e
Dave86dev <davidochando1986@gmail.com>
2023-05-24 17:24:28 +0200

    Nos cuenta la ficha blanca más de una vez.... bug a solucionar, resto hecho

--- js/main.js
+++ js/main.js
@@ -1,23 +1,25 @@
-
-
 let arrayGanador = [];
 
 //Funcion que genera la combinación ganadora.
 const generaGanadora = () => {
+  //De forma hardcodeada voy a inventarme los colores posibles...
+  let coloresPosibles = [
+    "rojo",
+    "verde",
+    "azul",
+    "amarillo",
+    "rosa",
+    "naranja",
+  ];
 
-    //De forma hardcodeada voy a inventarme los colores posibles...
-    let coloresPosibles = ["rojo", "verde", "azul", "amarillo", "rosa", "naranja"];
+  for (let i = 0; i < 4; i++) {
+    let posicionRandom = Math.floor(Math.random() * coloresPosibles.length);
 
-    for(let i = 0; i < 4; i++){
+    arrayGanador.push(coloresPosibles[posicionRandom]);
+  }
 
-        let posicionRandom = Math.floor(Math.random() * coloresPosibles.length);
-
-        arrayGanador.push(coloresPosibles[posicionRandom]);
-    }
-
-    console.log(coloresPosibles, "estos son los colores posibles...")
-    console.log(arrayGanador, "estos son los colores ganadores....")
-
+  // console.log(coloresPosibles, "estos son los colores posibles...")
+  // console.log(arrayGanador, "estos son los colores ganadores....")
 };
 
 generaGanadora();
@@ -27,42 +29,59 @@ let tablero = document.getElementById("tablero");
 let dificultad = 10;
 let contador = 1;
 
-//Esta parte acaca de ser hardcodeada, es decir, arbitrariamente hemos 
+//Esta parte acaca de ser hardcodeada, es decir, arbitrariamente hemos
 //decidido que tenga una dificultad de tipo fácil, dándole 10 líneas, naturalmente.
 
 const compruebaGanadora = () => {
+  //Hardcodeo la combinación por la que yo apuesto
+  let combinacionQueJuega = ["naranja", "rosa", "verde", "verde"];
 
+  let blancas = 0;
+  let negras = 0;
 
-}
+  console.log(combinacionQueJuega, " soy la que juega");
+  console.log(arrayGanador, " soy la que gana...");
+
+  for (let i = 0; i < arrayGanador.length; i++) {
+    if (arrayGanador.includes(combinacionQueJuega[i])) {
+      //El include saca la ficha blanca......
+
+      //Pero antes de darle la ficha blanca, comprobamos si es negra....
+      if (arrayGanador[i] === combinacionQueJuega[i]) {
+        //Esto es para ver si tenemos la ficha negra....
+        negras += 1;
+        console.log("he ganado..... ", negras, " fichas negras");
+
+        if (negras === 4) {
+          //Hemos ganado la partida...entonces nos vamos a otro html...
+          window.location.href = "../pages/winner.html";
+        }
+      } else {
+        blancas += 1;
+        console.log("tenemos ", blancas, " ficha blanca");
+      }
+    }
+  }
+};
 
 const pintaBola = (numeroDeFila) => {
-
-    console.log("has clickado en la fila.......", numeroDeFila)
-}
+  console.log("has clickado en la fila.......", numeroDeFila);
+};
 
 const pintaTablero = () => {
+  compruebaGanadora();
 
+  //Al no haber ganado nadie, el código continúa por aquí debajo...
 
-    compruebaGanadora();
+  if (dificultad > 1) {
+    document.getElementById(`fila${contador}`).removeAttribute("onclick");
 
-    //Al no haber ganado nadie, el código continúa por aquí debajo...
+    contador++;
 
-    if(dificultad > 1){
+    tablero.innerHTML += `<div id='fila${contador}' class='fila' onclick='pintaBola(${contador})'>${contador}</div>`;
 
-        document.getElementById(`fila${contador}`).removeAttribute("onclick");
-
-        contador++;
-
-        tablero.innerHTML += `<div id='fila${contador}' class='fila' onclick='pintaBola(${contador})'>${contador}</div>`;
-
-        dificultad -= 1;
-
-    } else {
-
-        console.log("you died!")
-    }
-
-
-}
-
-
+    dificultad -= 1;
+  } else {
+    console.log("you died!");
+  }
+};
